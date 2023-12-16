322f8c86b8029893943969e997aa61f75f99dfa9
Jo1ceLi <ms0615122@gmail.com>
2023-12-16 13:15:23 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	_ "fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	// Connect to RabbitMQ server
+	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a channel
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	// Your code here...
+}
